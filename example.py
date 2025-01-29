fce6a82574a73ad42f39d158b1fdf4a306c744a5
Vighnesh <vighneshshukla00@gmail.com>
2025-01-29 20:19:56 +0530

    Italic

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ root = tk.Tk()
 root.geometry("400x300")
 root.title("Hello")
 
-Label = tk.Label(root,text="Hello",font=("Cursive",18))
+Label = tk.Label(root,text="Hello",font=("Italic",18))
 Label.pack()
 
 
