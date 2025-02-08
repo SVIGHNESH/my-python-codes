9a2868b472554a7eb433050f9baf5980e627b8ba
Vighnesh <vighneshshukla00@gmail.com>
2025-02-08 16:18:40 +0530

    seeek

--- File_Operation.py
+++ File_Operation.py
@@ -5,13 +5,23 @@
 # data1 = f.readline()
 # print(data1)
 # f.close()
-f = open("/home/vighnesh/Desktop/Phython/File Operation/akshat.txt","w+")
-f.write("HELLO")
+
+
+# f = open("/home/vighnesh/Desktop/Phython/File Operation/akshat.txt","w+")
+# f.write("HELLO")
+# f.close()
+
+
+# d=open("/home/vighnesh/Desktop/Phython/File Operation/akshat.txt","r")
+# data1  = d.read()
+# print(data1)
+# d.close()
+
+f=open("/home/vighnesh/Desktop/Phython/File Operation/akshat.txt","w+")
+f.write("File opening in the xk;fjkf r+ mode")
+f.seek(0,0)
+data = f.read()
+
+print(data)
 f.close()
-
-
-d=open("/home/vighnesh/Desktop/Phython/File Operation/akshat.txt","r")
-data1  = d.read()
-print(data1)
-d.close()
 
