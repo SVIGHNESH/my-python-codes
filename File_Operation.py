409c0eab998aeec92599bcfc3cb7474f1e2e3045
Vighnesh <vighneshshukla00@gmail.com>
2025-02-08 15:57:43 +0530

    next file operatiion readline()

--- File_Operation.py
+++ File_Operation.py
@@ -1,4 +1,12 @@
 f = open("/home/vighnesh/Desktop/Phython/File Operation/akshat.txt","r")
 
-data = f.readline(0)
+data = f.read(25)
+f.seek(0,0)
 print(data)
+
+data1 = f.readline()
+print(data1)
+
+
+f.close()
+
