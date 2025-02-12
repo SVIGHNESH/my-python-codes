3be6b4b2b3be6c11edce16ef6446336adf55aa5b
Vighnesh <vighneshshukla00@gmail.com>
2025-02-12 19:45:18 +0530

    now it is completed the first gui project in tkinter

--- Tkinter-GUI/button_Tkinter.py
+++ Tkinter-GUI/button_Tkinter.py
@@ -2,9 +2,15 @@
 #button:you click it that it does stuff
 from tkinter import *
 
+count = 0
 def click():
      
-    print("Hello!!")
+    global count
+    count+=1
+    label.config(text=count)
+   
+    
+   
 
 window=Tk()
 
@@ -15,6 +21,15 @@ button.config(bg="#7f7")
 button.config(fg="#f77")
 button.config(activebackground='#7f7')
 button.config(activeforeground="#f77")
+image=PhotoImage(file="/home/vighnesh/Desktop/Phython/Tkinter-GUI/point_emoji.png")
+button.config(image=image)
+button.config(compound='left')
+#button.config(state=ACTIVE) # disable button (ACTIVE/DISABLED )
+label=Label(window,text=count)
+label.config(font=('Monospace',50))
+label.pack()
+
 button.pack()
+
 
 window.mainloop()
