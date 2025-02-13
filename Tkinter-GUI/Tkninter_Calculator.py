111f6cacd6a08e591c5f5a46a25b4d5accace91d
Vighnesh <vighneshshukla00@gmail.com>
2025-02-14 01:39:41 +0530

    DONE!

--- Tkinter-GUI/Tkninter_Calculator.py
+++ Tkinter-GUI/Tkninter_Calculator.py
@@ -1,14 +1,35 @@
 from tkinter import *
-def button_press():
-    pass
+def button_press(num):
+    global equation_text
+
+    equation_text = equation_text + str(num)
+    equation_label.set(equation_text)
+
 def equals():
-    pass
+    global equation_text
+
+    try:
+        total=str(eval(equation_text))
+        equation_label.set(total)
+
+        equation_text = total
+    except ZeroDivisionError:
+        equation_label.set("NOT DEFINED!")
+        equation_text=""
+
+    except SyntaxError:
+        equation_label.set("SYNTAX ERROR")
+        equation_text=""
+
 def clear():
-    pass
+    global equation_text
+
+    equation_label.set("")
+    equation_text=""
 
 window = Tk()
 window.title("Calculator")
-window.geometry("500x500")
+window.geometry("500x600")
 
 equation_text = ""
 
@@ -41,7 +62,7 @@ button3=Button(frame,
                height=4,
                width=9,
                font=35,
-               command=lambda:button_press(1))
+               command=lambda:button_press(3))
 button3.grid(row=0,column=2)
 
 button4=Button(frame,
@@ -49,7 +70,7 @@ button4=Button(frame,
                height=4,
                width=9,
                font=35,
-               command=lambda:button_press(1))
+               command=lambda:button_press(4))
 button4.grid(row=1,column=0)
 
 button5=Button(frame,
@@ -57,7 +78,7 @@ button5=Button(frame,
                height=4,
                width=9,
                font=35,
-               command=lambda:button_press(1))
+               command=lambda:button_press(5))
 button5.grid(row=1,column=1)
 
 button6=Button(frame,
@@ -65,7 +86,7 @@ button6=Button(frame,
                height=4,
                width=9,
                font=35,
-               command=lambda:button_press(1))
+               command=lambda:button_press(6))
 button6.grid(row=1,column=2)
 
 button7=Button(frame,
@@ -73,7 +94,7 @@ button7=Button(frame,
                height=4,
                width=9,
                font=35,
-               command=lambda:button_press(1))
+               command=lambda:button_press(7))
 button7.grid(row=2,column=0)
 
 button8=Button(frame,
@@ -81,7 +102,7 @@ button8=Button(frame,
                height=4,
                width=9,
                font=35,
-               command=lambda:button_press(1))
+               command=lambda:button_press(8))
 button8.grid(row=2,column=1)
 
 button9=Button(frame,
@@ -89,7 +110,7 @@ button9=Button(frame,
                height=4,
                width=9,
                font=35,
-               command=lambda:button_press(1))
+               command=lambda:button_press(9))
 button9.grid(row=2,column=2)
 
 button0=Button(frame,
@@ -97,8 +118,64 @@ button0=Button(frame,
                height=4,
                width=9,
                font=35,
-               command=lambda:button_press(1))
-button0.grid(row=3,columnspan=3)
+               command=lambda:button_press(0))
+button0.grid(row=3,column=0)
+
+plus=Button(frame,
+               text="+",
+               height=4,
+               width=9,
+               font=35,
+               command=lambda:button_press('+'))
+plus.grid(row=0,column=3)
+
+minus=Button(frame,
+               text="-",
+               height=4,
+               width=9,
+               font=35,
+               command=lambda:button_press('-'))
+minus.grid(row=1,column=3)
+
+multiply=Button(frame,
+               text="*",
+               height=4,
+               width=9,
+               font=35,
+               command=lambda:button_press('*'))
+multiply.grid(row=2,column=3)
+
+divide=Button(frame,
+               text="/",
+               height=4,
+               width=9,
+               font=35,
+               command=lambda:button_press('/'))
+divide.grid(row=3,column=3)
+
+equal=Button(frame,
+             text="=",
+             height=4,
+             width=9,
+             font=35,
+             command=equals)
+equal.grid(row=3,column=2)
+
+decimal=Button(frame,
+               text=".",
+               height=4,
+               width=9,
+               font=35,
+               command=lambda:button_press('.'))
+decimal.grid(row=3,column=1)
+
+clear=Button(frame,
+               text="CLEAR",
+               height=4,
+               width=21,
+               font=35,
+               command=clear)
+clear.grid(row=4,columnspan=4)
 
 
 window.mainloop()
