5b3b63a69d02fd0d4b02cd3a2e9fd6f83148f9dd
Vighnesh <vighneshshukla00@gmail.com>
2025-03-04 21:58:05 +0530

    hjkl;

--- conditionnals.py
+++ conditionnals.py
@@ -1,8 +1,9 @@
 a=int(input("Give the value of a:"))
 b=int(input("Give the value of b:"))
 if (a>b):
-    print(f"{a} is greater than {b}. ")ee if     
+    print(f"{a} is greater than {b}. ")
 else:
     print(f"{b} is greater than {a}.")
   
 
+#made of the function the fuction of the the thei hteuthethoeufhgto'ih
