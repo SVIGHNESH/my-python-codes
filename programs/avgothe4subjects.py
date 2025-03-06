fb6ea5acafc11a5866b117cf56da8d895472ff99
Vighnesh <vighneshshukla00@gmail.com>
2025-03-06 21:32:18 +0530

    jhgfds

--- programs/avgothe4subjects.py
+++ programs/avgothe4subjects.py
@@ -3,4 +3,7 @@ b=int(input("Put the marks of the subject two:"))
 c=int(input("Put the marks of the subject third:"))
 d=int(input("Put the marks of the subject fourth:"))
 e=(a+b+c+d)/4
+
 print("The avg. marks of the subjects:",e)
+
+#this is the program to find the average of the 4 numbers 
