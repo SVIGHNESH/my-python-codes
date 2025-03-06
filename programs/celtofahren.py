e8ecdb3b228e43e425a9fe42cce9b3a46aa5d824
Vighnesh <vighneshshukla00@gmail.com>
2025-03-06 21:33:00 +0530

    xfdsfcs

--- programs/celtofahren.py
+++ programs/celtofahren.py
@@ -1,3 +1,5 @@
 celsius = float(input("Enter temperature in Celsius: "))
 fahrenheit = (celsius * 9 / 5) + 32
 print("The fahrenheit conversion of the given number is-->",fahrenheit)
+
+#this is program to find the fahrenheit from the celsius temp.
