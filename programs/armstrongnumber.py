f26af2e4ecf1cbfc60eabc4b8f240b3a1b02b9a1
Vighnesh <vighneshshukla00@gmail.com>
2025-03-17 23:53:47 +0530

    rvrbj

--- programs/armstrongnumber.py
+++ programs/armstrongnumber.py
@@ -17,4 +17,5 @@ n =int (input("Enter the number:"))
 length = len(str(n))
 print(check_Armstrong(n))
 #this is program that shows whether the number is Armstrong 
-#program od the one 
+#program od th
+#thinf'lf eifmicm isn  in  if iw ie
