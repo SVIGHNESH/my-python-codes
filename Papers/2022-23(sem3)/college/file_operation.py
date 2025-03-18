c2de4ed0c3ba6b340935a42b303ef6d88a1e55c1
Vighnesh <vighneshshukla00@gmail.com>
2025-03-18 22:46:53 +0530

    dk

--- Papers/2022-23(sem3)/college/file_operation.py
+++ Papers/2022-23(sem3)/college/file_operation.py
@@ -1,3 +1,17 @@
 file = open("mint.txt","r")
-content  = file.readlines()
-print(content)
+content  = file.readline()
+while content:
+    print(content.strip())
+    content = file.readline()
+file.close()
+
+
+
+import os
+
+file_path="/home/vighnesh/Desktop/Phython/Papers/2022-23(sem3)/college/hello.txt"
+if os.path.exists(file_path):
+    os.remove(file_path)
+    print(f"{file_path} is successfully deleted.")
+else:
+    print(f"{file_path} is not exists.")
