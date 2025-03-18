8a098bcb43b18d2cb7407d5d18b351225d0ee93c
Vighnesh <vighneshshukla00@gmail.com>
2025-03-18 20:41:12 +0530

    SDFGHJK

--- Papers/2022-23(sem3)/college/change_the_position_of_first_and_last.py
+++ Papers/2022-23(sem3)/college/change_the_position_of_first_and_last.py
@@ -4,3 +4,22 @@ def change_str(s):
 str = "Hello"
 changed_str = change_str(str)
 print(changed_str)
+
+print()
+print()
+
+def triangle(N):
+    for i in range(1,N+1):
+        print("* "*i)
+triangle(4)
+
+print()
+print()
+def half_diamond(N):
+    for i in range(1,N+1):
+        print("* "*i)
+    for i in range(N-1,0,-1):
+        print("* "*i)
+
+
+half_diamond(6)
