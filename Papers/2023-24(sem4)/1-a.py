7891c7fc830166df215f4c0bece6b8f0ad3edba1
Vighnesh <vighneshshukla00@gmail.com>
2025-03-20 13:10:09 +0530

    xcvbnm

---
+++ Papers/2023-24(sem4)/1-a.py
@@ -0,0 +1,22 @@
+a = [1,2,3,4]
+b = [1,2,3,4]
+c = a 
+
+if a is b:
+    print("A is B")
+else:
+    print("False")
+
+if a == b:
+    print("a is equal to b")
+else:
+    print("A is not equal to B")
+if a is c:
+    print("A is C")
+else:
+    print("False")
+
+if a == c:
+    print("a is equal to c")
+else:
+    print("A is not equal to C")
