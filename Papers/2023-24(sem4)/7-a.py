e438f466470b505e3fac3e59974ab51cc004182d
Vighnesh <vighneshshukla00@gmail.com>
2025-03-20 14:49:24 +0530

    wfveg

---
+++ Papers/2023-24(sem4)/7-a.py
@@ -0,0 +1,6 @@
+import numpy as np
+random_numbers=np.random.randint(10,51,size=5)
+print(random_numbers)
+random_name=np.random.choice(["Ritik","Devang"],size = 1 )
+print(random_name)
+print(type(random_numbers))
