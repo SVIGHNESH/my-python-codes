66873cf1539dce9b687520b9b0d5ce3c0d3fdc21
Vighnesh <vighneshshukla00@gmail.com>
2025-04-03 23:36:04 +0530

    aefv

--- exponentiation.py
+++ exponentiation.py
@@ -3,3 +3,7 @@ b=int(input("Enter the degree:"))
 c=a**b
 print("The exponention the given of %s is %s"%(a,c)) 
 print("The eniuaifqjflka  wiope jf  ei  fiqji")
+#this isthe 
+#ijem
+#fpoefeifem
+#fmej
